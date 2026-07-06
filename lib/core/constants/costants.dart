9fd4e3f5e88a85fdfae0f5bb57cff1c4ed856be1
Abdul Rehman <a.rehman.nazar@gmail.com>
2026-07-06 15:05:23 +0500

    - Retrofit Integration for Web APIs - Dio as HTTP Client - clean architecture

---
+++ lib/core/constants/costants.dart
@@ -0,0 +1,4 @@
+const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String apiKey = '';
+const String country = '';
+const String catagory = '';
---
+++ lib/features/daily_news/data/repository/article_repository_imp.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:news_app/core/constants/costants.dart';
+import 'package:news_app/core/resources/data_state.dart';
+import 'package:news_app/features/daily_news/data/data_sources/remote/news_api_service.dart';
+import 'package:news_app/features/daily_news/data/models/article.dart';
+import 'package:news_app/features/daily_news/domain/entities/article.dart';
+import 'package:news_app/features/daily_news/domain/repository/article_repository.dart';
+
+class ArticleRepositoryImp implements ArticleRepository {
+  final NewsApiService _newsApiService;
+
+  ArticleRepositoryImp(this._newsApiService);
+
+  @override
+  Future<DataState<List<ArticleModel>>> getNewsArticle() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apiKey: apiKey,
+        country: country,
+        category: catagory,
+      );
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess((httpResponse.data));
+      } else {
+        return DataFailed(
+          DioException(
+                requestOptions: httpResponse.response.requestOptions,
+                error: httpResponse.response,
+                type: DioExceptionType.unknown,
+                message: httpResponse.response.statusMessage,
+              )
+              as List<ArticleModel>,
+        );
+      }
+    } on DioException catch (e) {
+      return DataFailed(e as List<ArticleModel>);
+    }
+  }
+}
